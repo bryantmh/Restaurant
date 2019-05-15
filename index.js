b79227d4b20640285fa04ac55d7dbd7b65150e7c
Bryant Hinton <bryanthinton01@gmail.com>
2019-05-15 13:00:23 -0600

    Changes

--- index.js
+++ index.js
@@ -18,49 +18,59 @@ const app = new Vue({
     el: '#app',
     data() {
       return data
-    },
-    router,
-    methods: {
+  },
+  router,
+  methods: {
       socketRun(){
         this.socket.onmessage = (event) => {
             var commandIn = JSON.parse(event.data);
             var retVal = execute(commandIn);
             if(retVal !== 'undefined' || typeof retVal != 'object'){
-              	console.log(retVal);
-            } else {
-				for (var i in retVal) {
-					switch(i){
-						case 'newgame':
-							games.push(retval[i]);
-							break;
-						case 'startgame':
-							alert('game started!');
-							break;
-						case 'playerjoined':
-							gameState.playerlist.push(retval[i]);
-							break;
-						case 'error':
-							console.log(retval[i]);
-							break;
-						default:
-							this[i] = retVal[i];
-					}
-					
-				}
+             console.log(retVal);
+         } else {
+            for (var i in retVal) {
+               switch(i){
+                case 'login':
+                    var dataOut = {
+                        senderId: null,
+                        command: 'GetGameList',
+                        data: null,
+                        gameId: null,
+                        recipientId: null
+                    };
+                    this.$parent.commandHandler(JSON.stringify(dataOut));
+                    break;
+                case 'newgame':
+                    games.push(retval[i]);
+                    break;
+                case 'startgame':
+                    alert('game started!');
+                    break;
+                case 'playerjoined':
+                    gameState.playerlist.push(retval[i]);
+                    break;
+                case 'error':
+                    console.log(retval[i]);
+                    break;
+                default:
+                    this[i] = retVal[i];
             }
-        }
-      },
-      commandHandler: function(data){
-          this.socket.send(data);
-      },
 
-    },
-    mounted() {
-      this.socket = new WebSocket("wss://echo.websocket.org");
-    },
-    watch: {
-      socket: function() {
-        this.socketRun();
-      }
+        }
     }
+}
+},
+commandHandler: function(data){
+  this.socket.send(data);
+},
+
+},
+mounted() {
+  this.socket = new WebSocket("wss://echo.websocket.org");
+},
+watch: {
+  socket: function() {
+    this.socketRun();
+}
+}
 });
