0501663feb0367c3eb24a55943cbd88fad299b5b
Hector Lopez <hectorlopeztrejo9@gmail.com>
2019-05-15 11:26:01 -0600

    changes to index.js

--- index.js
+++ index.js
@@ -27,36 +27,38 @@ const app = new Vue({
             var commandIn = JSON.parse(event.data);
             var retVal = execute(commandIn);
             if(retVal !== 'undefined' || typeof retVal != 'object'){
-              console.log(retVal);
+              	console.log(retVal);
             } else {
-              for (var i in retVal) {
-                switch(i){
-                  case 'newgame':
-                    games.push(retval[i]);
-                    continue;
-                  case 'mygame':
-                    gamestate = retval[i];
-                    var dataOut = {
-                      senderid: gamestate.gameowner,
-                      command: 'JoinGame',
-                      data: {'gameid': gamestate.gameid},
-                      gameid: gamestate.gameid,
-                      recipientid: null,
-                    }
-                    this.commandHandler(JSON.stringify(dataOut));
-                    continue;
-                  case 'startgame':
-                    alert('game started!');
-                    continue;
-                  case 'playerjoined':
-                    gamestate.playerlist.push(retval[i]);
-                    continue;
-                  case 'error':
-                    console.log(retval[i]);
-                    continue;
-                }
-                this[i] = retVal[i];
-              }
+				for (var i in retVal) {
+					switch(i){
+						case 'newgame':
+							games.push(retval[i]);
+							break;
+						case 'mygame':
+							gamestate = retval[i];
+							var dataOut = {
+								senderid: gamestate.gameowner,
+								command: 'JoinGame',
+								data: {'gameid': gamestate.gameid},
+								gameid: gamestate.gameid,
+								recipientid: null,
+							}
+							this.commandHandler(JSON.stringify(dataOut));
+							break;
+						case 'startgame':
+							alert('game started!');
+							break;
+						case 'playerjoined':
+							gamestate.playerlist.push(retval[i]);
+							break;
+						case 'error':
+							console.log(retval[i]);
+							break;
+						default:
+							this[i] = retVal[i];
+					}
+					
+				}
             }
         }
       },
