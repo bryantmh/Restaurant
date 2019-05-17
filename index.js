db7e2df03b3db530c80ae4d5e292238f674f8013
Hector Lopez <hectorlopeztrejo9@gmail.com>
2019-05-17 11:44:30 -0600

    stuff

--- index.js
+++ index.js
@@ -66,8 +66,10 @@ const app = new Vue({
                         case 'playerjoined':
                             // console.log(retVal[i]);
                             // console.log(JSON.parse(retVal[i]));
-                            var gameid = JSON.parse(retVal[i])['player']['gameId'];
-                            this.$set(this.games[gameid].playerList, JSON.parse(retVal[i])['player']['playerId'], JSON.parse(retVal[i])['player']);
+                            if(this.games !== 'undefined'){
+                                var gameid = JSON.parse(retVal[i])['player']['gameId'];
+                                this.$set(this.games[gameid].playerList, JSON.parse(retVal[i])['player']['playerId'], JSON.parse(retVal[i])['player']);
+                            }
                             this.$set(this.gameState.playerList, JSON.parse(retVal[i])['player']['playerId'], JSON.parse(retVal[i])['player']);
                             // this.gameState.playerList[JSON.parse(retVal[i])['player']['playerId']] = JSON.parse(retVal[i])['player'];
                             break;
