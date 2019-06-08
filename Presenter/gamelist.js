320e0db8c6827df529846dc6a1f6c33b85bee911
Bryant Hinton <bryanthinton01@gmail.com>
2019-06-07 19:40:12 -0600

    Fixed chat bug

--- Presenter/gamelist.js
+++ Presenter/gamelist.js
@@ -16,9 +16,28 @@ const gamelist = Vue.component('gamelist', {
         },
 
         joinGame(gameId) {
-            var messageData = JSON.stringify({'gameId': gameId, 'screenName': this.inputName});
-            var message = new Message('JoinGame', messageData, this.clientId, gameId).toString();
-            this.serverProxy.commandHandler(message);
+            var willSend = true;
+            for (var game in this.games) {
+                if (this.games[game].gameId == gameId) {
+                    for (var user in this.games[game].players) {
+                        if (this.games[game].players[user].screenName == this.inputName) {
+                            willSend = false;
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            console.log(willSend);
+            if (willSend) {
+                var messageData = JSON.stringify({'gameId': gameId, 'screenName': this.inputName});
+                var message = new Message('JoinGame', messageData, this.clientId, gameId).toString();
+                this.serverProxy.commandHandler(message);
+            }
+            else {
+                this.message = "Your name has already been taken. Please select another";
+            }
+          
         },
     },
 
