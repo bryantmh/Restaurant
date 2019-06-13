0db79d6abe464f3d3fca9b3e67ccafc2e5f2e788
Hector Lopez <hectorlopeztrejo9@gmail.com>
2019-06-13 00:16:57 -0600

    client reconnects to web socket automatically

--- ClientCommunicator/ServerProxy.js
+++ ClientCommunicator/ServerProxy.js
@@ -12,7 +12,22 @@ class ServerProxy {
 	        if (newMessage !== undefined) {
 	        	this.commandHandler(newMessage);
 	        }
-	    }
+		}
+		this.socket.onclose = (event) => {
+			console.log("Socket is closed. Reconnecting...");
+			setTimeout(function(e){
+				console.log('Attempting to reconnect...');
+				this.socket = new WebSocket('ws://jaredhammon.com:8080/ticket-to-ride-back-end/');
+				console.log('Reconnect successful!');
+				this.socket.socketRun();
+				this.socket.send("Reconnecting");
+			}, 5000);
+		}
+
+		this.socket.onerror = function(error){
+			console.error("Socket encountered error: ", error.message, "Closing socket");
+			this.socket.close();
+		}
 	}
 
 	commandHandler(data) {
